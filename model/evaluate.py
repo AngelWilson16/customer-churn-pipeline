d3506a13fecbe86a6312bd14842c9d3b683d5c29
AngelWilson16 <wilsonangel871@gmail.com>
2025-08-25 11:13:35 +0530

    Initial Commit 1.1

---
+++ model/evaluate.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+# Load the dataset
+
+data = pd.read_csv("C:\customer-churn-pipeline\data\WA_Fn-UseC_-Telco-Customer-Churn.csv")
+
+# Preprocess the dataset
+X = data.drop('Churn', axis=1)
+y = data['Churn']
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, 
+test_size=0.2, random_state=42)
+
+# Load the saved model
+model = joblib.load('model/customer_churn_model.pkl')
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Model accuracy: {accuracy:.2f}')
+
