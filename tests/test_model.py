dcd8b617ce3bca208aa91407321b5eac95db469e
AngelWilson16 <wilsonangel871@gmail.com>
2025-08-31 17:52:38 +0530

    Initial Commit 1.1

--- tests/test_model.py
+++ tests/test_model.py
@@ -6,9 +6,10 @@ class TestModelTraining(unittest.TestCase):
     def test_model_training(self):
         model = joblib.load('model/customer_churn_model.pkl')
         self.assertIsInstance(model, RandomForestClassifier)
-        self.assertGreaterEqual(len(model.feature_importance_), 4)
-if__name__ == '__main__':
-   unittest.main()
+        self.assertGreaterEqual(len(model.feature_importances_), 4)
+
+if __name__ == '__main__':
+    unittest.main()
 
         
 
